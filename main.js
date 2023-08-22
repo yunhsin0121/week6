55f28e205c75ee42dd107bad4c2ecd3cf99d3a7c
yunhsin <alice334830@gmail.com>
2023-08-22 22:53:27 +0800

    add index.html

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
 console.log("Hello world!");
+
+const swiper = new Swiper(".mySwiper", {
+    allowTouchMove: false,
+    slidesPerView: "auto",
+    speed: 20000,
+    loop: true,
+    autoplay: {
+      delay: 0,
+    }
+  });
